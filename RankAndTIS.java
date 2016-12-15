810165a2485963ccb89165da37035b243b56a2cd
Mike <mweinberg21@gmail.com>
2016-12-14 19:34:33 -0600

    Update RankAndTIS.java

--- RankAndTIS.java
+++ RankAndTIS.java
@@ -11,14 +11,15 @@ import java.util.Calendar;
     // TODO Create a new class with a different name. With blackjack, and hookers
 
 
+
     // Static methods for Rank strings and Time in Service operations
     // Days in month method is here too because I got sick of looking at it in Add_Orders Activity code
 
 
 public class RankAndTIS {
 
+
     // Returns Time in Service in Years
-    // TODO More Efficient Way to Do This
     public static int getYearsInService(int month, int year) {
 
         // Get current date
@@ -26,6 +27,7 @@ public class RankAndTIS {
         int CurrentYear = now.get(Calendar.YEAR);
         int CurrentMonth = now.get(Calendar.MONTH);
 
+        // Figure out the difference
         int diff = CurrentYear - year;
         if (month > CurrentMonth) diff--;
 
@@ -34,82 +36,146 @@ public class RankAndTIS {
 
 
     // Returns Pay for Rank and TIS
+    // Yeah, its a bit much but it would take awhile to make an XML, also would take more juice to read an XML anyway
     public static double getPayPerMuta(int rank, int TIS) {
 
-        // TODO Write this!!
+        // 2016 Paychart used below. Update for 2017 once available
 
-        return 10.25;
+        double returnVal = 0.00;
+
+        // E1
+        if (rank == 0) returnVal = 1566.90;
+
+        // E2
+        else if (rank == 1) returnVal = 1756.50;
+
+        // E3
+        else if (rank == 2) {
+            if (TIS < 2) returnVal = 1847.10;
+            else if (TIS >= 2 && TIS < 3) returnVal = 1963.20;
+            else if (TIS >= 3) returnVal = 2082.00;
+        }
+
+        // E4
+        else if (rank == 3) {
+            if (TIS < 2) returnVal = 2046.00;
+            else if (TIS >= 2 && TIS < 3) returnVal = 2150.40;
+            else if (TIS >= 3 && TIS < 4) returnVal = 2267.10;
+            else if (TIS >= 4 && TIS < 6) returnVal = 2382.00;
+            else if (TIS >= 6) returnVal = 2483.40;
+        }
+
+        // E5
+        else if (rank == 4) {
+            if (TIS < 2) returnVal = 2231.40;
+            else if (TIS >= 2 && TIS < 3) returnVal = 2381.40;
+            else if (TIS >= 3 && TIS < 4) returnVal = 2496.60;
+            else if (TIS >= 4 && TIS < 6) returnVal = 2614.20;
+            else if (TIS >= 6 && TIS < 8) returnVal = 2797.80;
+            else if (TIS >= 8 && TIS < 10) returnVal = 2989.80;
+            else if (TIS >= 10 && TIS < 12) returnVal = 3147.60;
+            else if (TIS >= 12) returnVal = 3166.20;
+        }
+
+        // E6
+        else if (rank == 5) {
+            if (TIS < 2) returnVal = 2435.70;
+            else if (TIS >= 2 && TIS < 3) returnVal = 2680.20;
+            else if (TIS >= 3 && TIS < 4) returnVal = 2798.40;
+            else if (TIS >= 4 && TIS < 6) returnVal = 2913.60;
+            else if (TIS >= 6 && TIS < 8) returnVal = 3033.60;
+            else if (TIS >= 8 && TIS < 10) returnVal = 3303.30;
+            else if (TIS >= 10 && TIS < 12) returnVal = 3408.60;
+            else if (TIS >= 12 && TIS < 14) returnVal = 3612.30;
+            else if (TIS >= 14 && TIS < 16) returnVal = 3674.40;
+            else if (TIS >= 16 && TIS < 18) returnVal = 3719.70;
+            else if (TIS >= 18) returnVal = 3772.50;
+        }
+
+        // E7
+        else if (rank == 6) {
+            if (TIS < 2) returnVal = 2816.10;
+            else if (TIS >= 2 && TIS < 3) returnVal = 3073.50;
+            else if (TIS >= 3 && TIS < 4) returnVal = 3191.40;
+            else if (TIS >= 4 && TIS < 6) returnVal = 3347.10;
+            else if (TIS >= 6 && TIS < 8) returnVal = 3468.90;
+            else if (TIS >= 8 && TIS < 10) returnVal = 3678.00;
+            else if (TIS >= 10 && TIS < 12) returnVal = 3795.60;
+            else if (TIS >= 12 && TIS < 14) returnVal = 4004.70;
+            else if (TIS >= 14 && TIS < 16) returnVal = 4178.70;
+            else if (TIS >= 16 && TIS < 18) returnVal = 4297.50;
+            else if (TIS >= 18 && TIS < 20) returnVal = 4423.80;
+            else if (TIS >= 20 && TIS < 22) returnVal = 4472.70;
+            else if (TIS >= 22 && TIS < 24) returnVal = 4637.10;
+            else if (TIS >= 24 && TIS < 26) returnVal = 4725.30;
+            else if (TIS >= 26) returnVal = 5061.30;
+        }
+
+
+
+        /*  TEMPLATE FOR LATER RANKS IF I FEEL LIKE IT
+        // E7
+        else if (rank == 6) {
+            if (TIS < 2) returnVal = 2816.10;
+            else if (TIS >= 2 && TIS < 3) returnVal = 3073.50;
+            else if (TIS >= 3 && TIS < 4) returnVal = 3191.40;
+            else if (TIS >= 4 && TIS < 6) returnVal = 3347.10;
+            else if (TIS >= 6 && TIS < 8) returnVal = 3468.90;
+            else if (TIS >= 8 && TIS < 10) returnVal = 3678.00;
+            else if (TIS >= 10 && TIS < 12) returnVal = 3795.60;
+            else if (TIS >= 12 && TIS < 14) returnVal = 4004.70;
+            else if (TIS >= 14 && TIS < 16) returnVal = 4178.70;
+            else if (TIS >= 16 && TIS < 18) returnVal = 4297.50;
+            else if (TIS >= 18 && TIS < 20) returnVal = 4423.80;
+            else if (TIS >= 20 && TIS < 22) returnVal = 4472.70;
+            else if (TIS >= 22 && TIS < 24) returnVal = 4637.10;
+            else if (TIS >= 24 && TIS < 26) returnVal = 4725.30;
+            else if (TIS >= 26) returnVal = 5061.30;
+        }
+        */
+
+
+        return (returnVal / 30); // 1 MUTA = 1/30 basic pay for a month. DoD releases pay charts by month
     }
 
 
 
     // Returns Rank String for Rank Code
-    // TODO Replace below code with switch
     public static String rankForCode(int rank) {
-
-        // TODO Integrate this with Resources somehow
-
-        String returnVal = "";
-        if (rank == 0) returnVal = "PVT";
-        else if (rank == 1) returnVal = "PV2";
-        else if (rank == 2) returnVal = "PFC";
-        else if (rank == 3) returnVal = "SPC";
-        else if (rank == 4) returnVal = "SGT";
-        else if (rank == 5) returnVal = "SSG";
-        else if (rank == 6) returnVal = "SFC";
-        return returnVal;
+        switch (rank) {
+            case 0: return "PVT";
+            case 1: return "PV2";
+            case 2: return "PFC";
+            case 3: return "SPC";
+            case 4: return "SGT";
+            case 5: return "SSG";
+            case 6: return "SFC";
+            default: return "";
+        }
     }
 
 
-    // Static method for returning the month string from month number
-    // TODO Replace below code with Switch
+    // Static method for returning the 3 letter month string from month number
     public static String getStringForMonth(int month) {
-
-        // TODO Integrate this with Resources somehow
-
-        // January
-        if (month == 0) return "Jan";
-
-            // February
-        else if (month == 1) return "Feb";
-
-        // March
-        else if (month == 2) return "Mar";
-
-            // April
-        else if (month == 3) return "Apr";
-
-            // May
-        else if (month == 4) return "May";
-
-            // June
-        else if (month == 5) return "Jun";
-
-            // July
-        else if (month == 6) return "Jul";
-
-            // August
-        else if (month == 7) return "Aug";
-
-            // September
-        else if (month == 8) return "Sep";
-
-            // October
-        else if (month == 9) return "Oct";
-
-            // November
-        else if (month == 10) return "Nov";
-
-            // December
-        else if (month == 11) return "Dec";
-
-        else return "";
-
+        switch (month) {
+            case 0: return "Jan";
+            case 1: return "Feb";
+            case 2: return "Mar";
+            case 3: return "Apr";
+            case 4: return "May";
+            case 5: return "Jun";
+            case 6: return "Jul";
+            case 7: return "Aug";
+            case 8: return "Sep";
+            case 9: return "Oct";
+            case 10: return "Nov";
+            case 11: return "Dec";
+            default: return "";
+        }
     }
 
 
     // Static method for determining the amount of days in a month
-    // TODO Replace below code with Switch
     public static int daysInMonth(int month, int year) {
 
         // January
@@ -158,7 +224,6 @@ public class RankAndTIS {
 
 
 
-    // TODO Comment this shit out so noobs can follow
     public static int getMUTAsFor(int ordersType, int[] startDate, int[] endDate) {
 
         // Orders Type 0 indicates MFH duty. Only 1 MUTA possible. Avoid all the kerfuffle and end this now.
